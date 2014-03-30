f9ccadf0914dbbbb5f14186c23dc18ba0af94d9d
show0925 <show0925@163.com>
2014-03-30 22:54:53 +0800

    fdsaf

---
+++ GXService/GXService.Broadcast.Contract/Command.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace GXService.Broadcast.Contract
+{
+    [DataContract(Namespace = "GXService.Broadcast.Contract")]
+    public class Command
+    {
+        [DataMember]
+        public CommandType CommandType { get; set; }
+
+        [DataMember]
+        public byte[] Data { get; set; }
+    }
+}
---
+++ GXService/GXService.Broadcast.Contract/CommandType.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace GXService.Broadcast.Contract
+{
+    [DataContract(Namespace = "GXService.Broadcast.Contract")]
+    public enum CommandType
+    {
+        数据广播,
+        开始识别
+    }
+}
